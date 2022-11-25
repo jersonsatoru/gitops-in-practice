23af13b455a366c4330e20ffa4b7ad7cfc5e4628
jersonsatoru <jersonsatoru@yahoo.com.br>
2022-11-25 17:22:11 -0300

    chore(update_cluster): remove echo statement

--- scripts/update_cluster.sh
+++ scripts/update_cluster.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 for d in k8s/projects/*; do
-    project_name=$(echo $d | sed -e  s!k8s\/projects\/!!g)
     kubectl apply -f "${d}"
-    kubectl rollout restart deployment "${project_name}-deployment"
-    echo "$project_name"
+    project_name=$(echo $d | sed -e  s!k8s\/projects\/!!g)
+    kubectl rollout restart deployment "${project_name}-deployment" -n local
 done
 
 kubectl apply -f k8s/ingress.yaml
