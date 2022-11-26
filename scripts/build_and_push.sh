eb92c75fb69ab25c41488b0be90732f21f8709ab
jersonsatoru <jersonsatoru@yahoo.com.br>
2022-11-25 22:44:46 -0300

    chore(buid_and_push.sh): update directory structure

--- scripts/build_and_push.sh
+++ scripts/build_and_push.sh
@@ -2,9 +2,9 @@
 
 LOCAL_REGISTRY="localhost:5001"
 
-for d in projects/*; do
-    project_name=$(echo $d | sed -e  s!projects\/!!g)
+for d in ../projects/*; do
+    project_name=$(echo $d | sed -e  s!..\/projects\/!!g)
     image_name=$LOCAL_REGISTRY/${project_name}
-    docker build --no-cache -t $image_name ./projects/${project_name}
+    docker build --no-cache -t $image_name ../projects/${project_name}
     docker push $image_name
 done
